dcd650bbf7b5ef04436b59f000e7156d1ce7e087
ohminsun <ohminsun@gmail.com>
2015-06-22 12:12:40 -0500

    added views for header and footer and also added css stylesheets bootstrap and jquery

---
+++ application/views/inc/footer_view.php
@@ -0,0 +1,6 @@
+<footer>
+	&copy; <?mdate('Y')?>
+</footer>
+
+</body>
+</html>
---
+++ application/views/inc/header_view.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en"></html>
+<head>
+	<title>Test</title>
+	<link rel="stylesheet" href="<?mbase_uri()?>public/css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="<?mbase_uri()?>public/css/style.css"/>
+	<script src="<?mbase_uri()?>public/js/jquery.js"></script>
+	<script src="<?mbase_uri()?>public/js/bootstrap.js"></script>
+
+</head>
+<body>
+<header>
+	jrDash
+</header>
+</body>
+</html>
