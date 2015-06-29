6ef93d3c871f1df1586b5d97f2183f7360207972
ohminsun <ohminsun@gmail.com>
2015-06-29 09:41:49 -0500

    updated pulling and pushing to db file

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -9,11 +9,28 @@ class Home extends CI_Controller
 		echo "Home Page";
 	}
 
-	public function test()
+	// public function test()
 	{
+		/*	SELECT user_id, login FROM user
+			ORDER BY user_id DESC */
+		$this->db->select('user_id, login');
+		$this->db->from('user');
 		$this->db->order_by(['user_id DESC']);
-		$q = $this->db->get('user');
+
+		$q = $this->db->get();
 		print_r($q->result());
-	}
+
+		/* $this->db->insert('user', [
+			'login' ==> 'Jenkins'
+		]); */
+
+		/* $this->db->where('['user_id' => 4]);
+		$this->db->update('user', [
+			'login' => 'Sammy'
+		]); */
+		
+		/* $this->db->delete('user', ['user_id' => 3]); */
+
+	// }
 }
 ?>
